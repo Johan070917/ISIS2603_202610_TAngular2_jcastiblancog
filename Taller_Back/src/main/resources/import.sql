4e1577db5e1d10f71a88a32fa53c73e96ba266be
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2026-04-26 23:21:56 -0500

    Initial Commit
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

---
+++ Taller_Back/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO country_entity (id, name, iso_code) VALUES (1, 'Colombia', 'CO');
+INSERT INTO country_entity (id, name, iso_code) VALUES (2, 'United Kingdom', 'UK');
+INSERT INTO country_entity (id, name, iso_code) VALUES (3, 'Japan', 'JP');
+INSERT INTO country_entity (id, name, iso_code) VALUES (4, 'Germany', 'DE');
+INSERT INTO country_entity (id, name, iso_code) VALUES (5, 'Oceania', 'OC');
+
+INSERT INTO city_entity (id, name, country_id) VALUES (1, 'Bogota', 1);
+INSERT INTO city_entity (id, name, country_id) VALUES (2, 'London', 2);
+INSERT INTO city_entity (id, name, country_id) VALUES (3, 'London', 4);
+INSERT INTO city_entity (id, name, country_id) VALUES (4, 'London', 3);
